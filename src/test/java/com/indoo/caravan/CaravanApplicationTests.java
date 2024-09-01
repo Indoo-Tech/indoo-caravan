dc1b24e5ab17777cd6961616980d9c0c913206fb
Mudabir Hussain <mudabirburfat97@gmail.com>
2024-09-01 15:00:27 +0400

    Initial Commit

---
+++ src/test/java/com/indoo/caravan/CaravanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indoo.caravan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
